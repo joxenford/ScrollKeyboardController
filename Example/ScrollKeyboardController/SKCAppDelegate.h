d58f76f740cf3be35a8d8e3c62835c2fdfb37738
Brad Morgan <brad@dmgctrl.com>
2014-11-13 18:07:44 -0500

    Removed unused code

--- Example/ScrollKeyboardController/SKCAppDelegate.h
+++ Example/ScrollKeyboardController/SKCAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  SKCAppDelegate.h
-//  ScrollKeyboardController
-//
-//  Created by CocoaPods on 11/13/2014.
-//  Copyright (c) 2014 Brad Morgan. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SKCAppDelegate : UIResponder <UIApplicationDelegate>
--- Example/ScrollKeyboardController/SKCViewController.h
+++ Example/ScrollKeyboardController/SKCViewController.h
@@ -1,11 +1,3 @@
-//
-//  SKCViewController.h
-//  ScrollKeyboardController
-//
-//  Created by Brad Morgan on 11/13/2014.
-//  Copyright (c) 2014 Brad Morgan. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SKCViewController : UIViewController
